04b65d24eab7e24c6710fab2305fc7668d824890
Karim Mohamed <komicho1996@gmail.com>
2023-08-23 17:30:39 +0300

    Feat: Check grid has rows

--- resources/views/builder/build/grid.blade.php
+++ resources/views/builder/build/grid.blade.php
@@ -41,44 +41,68 @@
             <div class="row row-cards">
                 <div class="col-12">
                     <div class="card mb-3">
-                        <div class="table-responsive">
 
-                            <table class="table table-vcenter card-table">
-                                <thead>
-                                    <tr>
-                                        @foreach ($grid->getLabels() as $label)
-                                            <th>{{ $label }}</th>
-                                        @endforeach
-                                        <th width="15%"></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($grid->getRows() as $row)
+                        @if ($grid->getRows()->isEmpty())
+                            <div class="empty">
+                                <div class="empty-icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                        class="icon icon-tabler icon-tabler-mood-empty" width="24" height="24"
+                                        viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                        stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+                                        <path d="M9 10l.01 0"></path>
+                                        <path d="M15 10l.01 0"></path>
+                                        <path d="M9 15l6 0"></path>
+                                    </svg>
+                                </div>
+                                <p class="empty-title">No results found</p>
+                                <p class="empty-subtitle text-secondary">
+                                    Try adjusting your search or filter or creating a new record to find a records.
+                                </p>
+                            </div>
+                        @endif
+
+                        @if (!$grid->getRows()->isEmpty())
+                            <div class="table-responsive">
+
+                                <table class="table table-vcenter card-table">
+                                    <thead>
                                         <tr>
-                                            @foreach ($row['columns'] as $column)
-                                                <td>{!! $column !!}</td>
+                                            @foreach ($grid->getLabels() as $label)
+                                                <th>{{ $label }}</th>
                                             @endforeach
-                                            <td align="right">
-                                                @foreach ($row['calls'] as $call => $action)
-                                                    @if ($action['type'] == 'route')
-                                                        <a class="btn btn-sm btn-{{ $action['color'] ?? 'primary' }}"
-                                                            href="{{ route($action['route'], [$row['primary']]) }}">
-                                                            {{ \Illuminate\Support\Str::contains($call, '::grids') ? __($call) : $call }}
-                                                        </a>
-                                                    @endif
-                                                @endforeach
-
-                                                @if ($canRemoveRow)
-                                                    <button class="btn btn-sm btn-danger"
-                                                        wire:click="removeRow({{ $row['primary'] }})">Remove</button>
-                                                @endif
-                                            </td>
+                                            <th width="15%"></th>
                                         </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($grid->getRows() as $row)
+                                            <tr>
+                                                @foreach ($row['columns'] as $column)
+                                                    <td>{!! $column !!}</td>
+                                                @endforeach
+                                                <td align="right">
+                                                    @foreach ($row['calls'] as $call => $action)
+                                                        @if ($action['type'] == 'route')
+                                                            <a class="btn btn-sm btn-{{ $action['color'] ?? 'primary' }}"
+                                                                href="{{ route($action['route'], [$row['primary']]) }}">
+                                                                {{ \Illuminate\Support\Str::contains($call, '::grids') ? __($call) : $call }}
+                                                            </a>
+                                                        @endif
+                                                    @endforeach
 
-                        </div>
+                                                    @if ($canRemoveRow)
+                                                        <button class="btn btn-sm btn-danger"
+                                                            wire:click="removeRow({{ $row['primary'] }})">Remove</button>
+                                                    @endif
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+
+                            </div>
+                        @endif
                     </div>
 
                     {{ $grid->getLinks() }}
