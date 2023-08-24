8ce351ade713b4b5ec1aa9fc96bde49ba241ab6c
Karim Mohamed <komicho1996@gmail.com>
2023-08-24 09:53:38 +0300

    Feat: enable add force color for the amount

--- src/Views/Components/Amount.php
+++ src/Views/Components/Amount.php
@@ -6,14 +6,18 @@ use Illuminate\View\Component;
 
 class Amount extends Component
 {
-    public $thanZero = null;
+    public $color = null;
 
-    public function __construct(public $value)
+    public function __construct(public $value, public $setColor = null)
     {
         $this->value = number_format($value, 2);
 
         if ($value != 0)
-            $this->thanZero = ($value > 0) ? 'green' : 'red';
+            $this->color = ($value > 0) ? 'green' : 'red';
+
+        if ($setColor) {
+            $this->color = $setColor;
+        }
     }
 
     /**
@@ -22,7 +26,7 @@ class Amount extends Component
     public function render()
     {
         return <<<'BLADE'
-            <span style="font-family: monospace;" class="text-{{ $thanZero ?? 'orange' }}">{{ $value }} EGP</span>
+            <span style="font-family: monospace;" class="text-{{ $color ?? 'orange' }}">{{ $value }} EGP</span>
         BLADE;
     }
 }
