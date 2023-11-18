14fbd0eede3de1c30d49d9155d722aa53d850b9c
Karim Mohamed <komicho1996@gmail.com>
2023-11-18 23:05:05 +0200

    Feat: add permission checking to the navbar

--- resources/views/builder/navbar.blade.php
+++ resources/views/builder/navbar.blade.php
@@ -1,5 +1,5 @@
 @foreach ($links as $link)
-    @if (isset($link['href']))
+    @if (isset($link['href']) && hasPermission($link['permission']))
         <li class="nav-item">
             <a class="nav-link" href="{{ route($link['href']) }}">
                 <span class="nav-link-icon d-md-none d-lg-inline-block">
@@ -27,7 +27,7 @@
         </li>
     @endif
 
-    @if (isset($link['sublinks']))
+    @if (isset($link['sublinks']) && hasPermission($link['permission']))
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#navbar-help" data-bs-toggle="dropdown"
                 data-bs-auto-close="outside" role="button" aria-expanded="false">
@@ -35,9 +35,9 @@
                     @if (isset($link['icon']))
                         @svg('tabler-' . $link['icon'], 'icon')
                     @else
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon"
-                            width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                             <path d="M4 8v-2a2 2 0 0 1 2 -2h2"></path>
                             <path d="M4 16v2a2 2 0 0 0 2 2h2"></path>
@@ -55,12 +55,14 @@
             </a>
             <div class="dropdown-menu">
                 @foreach ($link['sublinks'] as $_link)
-                    <a class="dropdown-item" href="{{ route($_link['href']) }}">
-                        @if (isset($_link['icon']))
-                            @svg('tabler-' . $_link['icon'], 'icon me-1')
-                        @endif
-                        {{ \Illuminate\Support\Str::contains($_link['label'], '::navbar') ? __($_link['label']) : $_link['label'] }}
-                    </a>
+                    @if (hasPermission($_link['permission']))
+                        <a class="dropdown-item" href="{{ route($_link['href']) }}">
+                            @if (isset($_link['icon']))
+                                @svg('tabler-' . $_link['icon'], 'icon me-1')
+                            @endif
+                            {{ \Illuminate\Support\Str::contains($_link['label'], '::navbar') ? __($_link['label']) : $_link['label'] }}
+                        </a>
+                    @endif
                 @endforeach
             </div>
         </li>
