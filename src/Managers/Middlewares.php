ac7cb0638c251af6e838506295ac19f4b11d3875
Karim Mohamed <komicho1996@gmail.com>
2024-05-02 03:54:52 +0300

    Feat: enable sorting middlewares

--- src/Managers/Middlewares.php
+++ src/Managers/Middlewares.php
@@ -7,17 +7,39 @@ use Obelaw\Framework\Pipeline\Locale\Http\Middleware\LocaleMiddleware;
 class Middlewares
 {
     private $middlewares = [
-        'web',
-        LocaleMiddleware::class
+        [
+            'sort' => 1,
+            'middleware' => 'web',
+        ],
+        [
+            'sort' => 999,
+            'middleware' => LocaleMiddleware::class,
+        ],
     ];
 
-    public function addMiddleware($middleware): void
+    public function addMiddleware($middleware, int $sort = 999): void
     {
-        array_push($this->middlewares, $middleware);
+        $middleware = [
+            [
+                'sort' => $sort,
+                'middleware' => $middleware,
+            ]
+        ];
+
+        $collection = collect($this->middlewares);
+        $collection = $collection->merge($middleware);
+
+        $this->middlewares = $collection->all();
     }
 
     public function getMiddlewares(): array
     {
-        return $this->middlewares;
+        $collection = collect($this->middlewares);
+        $collection = $collection->sortBy('sort');
+        $collection = $collection->map(function (array $item, int $key) {
+            return $item['middleware'];
+        });
+
+        return $collection->all();
     }
 }
